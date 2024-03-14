a6bbc96c81a037647c9b01c9b6199a59d6f51795
Livia <128159882+liviaa06@users.noreply.github.com>
2024-03-14 12:27:24 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+//let x = "";
+//console.log(x);
+//x = "oi";
+
+//function imprimeTexto(texto) {
+    //console.log(texto)
+//}
+
+//imprimeTexto("oi");
+//imprimeTexto("outro texto");
+
+
+function soma(){
+    const resultado = 2 + 2;
+}
+console.log(soma());
